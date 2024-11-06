27944f0c183ae32663e146e48d2013d1808db84e
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-11-06 12:45:49 +0100

    oop update

--- OOP_CS50_Harvard/student.py
+++ OOP_CS50_Harvard/student.py
@@ -1,1 +1,34 @@
-class S
+class Students:
+    def __init__(self, name, house, spell):
+        if not name:
+            raise ValueError("Missing Name")
+        if house not in ['Gdynia', 'Sopot', 'Gdansk']:
+            raise ValueError('Wrong house')
+        self.name = name
+        self.house = house
+        self.spell = spell
+
+    def __str__(self):
+        return f"{self.name} from {self.house} and {self.spell}"
+
+    def charm(self):
+        match self.spell:
+            case 'Lumos':
+                return 'Light'
+            case 'Patronus':
+                return 'Animal'
+            case _:
+                return 'No spell'
+
+def main():
+    student = get_student()
+    print(f"Spell {student.charm()}")
+
+def get_student():
+    name = input('What is the name: ')
+    house = input('What is the house: ')
+    spell = input('Favourit spell: ')
+    return Students(name, house, spell)
+
+if __name__ == '__main__':
+    main()
