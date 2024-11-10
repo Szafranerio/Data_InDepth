e1b0467db29940fc7b947fa9ff2e3cb9775ed119
Bartlomiej Szafran <bartekszafran@icloud.com>
2024-11-10 15:11:42 +0100

    update oop

--- OOP_CS50_Harvard/restaurant.py
+++ OOP_CS50_Harvard/restaurant.py
@@ -4,12 +4,23 @@ class Restaurant():
         self.cusine_type = cusine_type
         self.open = open
         self.numbered_served = 0
-    
+
     def describe_restaurant(self):
         print(f'{self.restaurant_name.title()}, this is {self.cusine_type.title()} restaurant and is open daily {self.open}, cusotmer served {self.numbered_served}')
-        
+
     def set_number_served(self, number):
         self.numbered_served += number
+
+
+class IceCreams(Restaurant):
+    def __init__(self, restaurant_name, cusine_type, open):
+        super().__init__(restaurant_name, cusine_type, open)
+        self.flavors = ['Strawberry', 'Chocolate', 'Lemon']
+
+    def show_flavors(self):
+        print('Iceee have several flavors such as:')
+        print(self.flavors)
+
 
 amici = Restaurant('Amici', 'Italian', 'from 16-20')
 fusion = Restaurant('Fusion', 'Danish', 'from 14-22')
@@ -21,3 +32,8 @@ kro.set_number_served(123)
 amici.describe_restaurant()
 fusion.describe_restaurant()
 kro.describe_restaurant()
+
+my_ice_creams = IceCreams('Iceee', 'Deserts', 'from 10-18')
+my_ice_creams.set_number_served(10)
+my_ice_creams.describe_restaurant()
+my_ice_creams.show_flavors()
